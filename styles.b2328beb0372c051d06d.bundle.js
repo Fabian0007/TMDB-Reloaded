8629cbb03a6740ca1d8f6cce9a81bad93a6ae017
fabian0007 <fabian.cano@correounivalle.edu.co>
2016-12-15 19:14:18 +0000

    new gh-pages version

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{457:function(n,o){},832:function(n,o,c){n.exports=c(457)}},[832]);
+webpackJsonp([1,2],{458:function(n,o){},835:function(n,o,c){n.exports=c(458)}},[835]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
